f0ee10c31d3641abc733c07a0d0b18ef269030ac
J0onYEong <cjy2134@naver.com>
2024-08-15 02:16:14 +0900

    [CAP-002] CAPTabBarItemView구현

---
+++ Modules/Presentation/DSKit/Source/CommonUI/TabBarItem/CAPTabBarItemView.swift
@@ -0,0 +1,114 @@
+//
+//  CAPTabBarItemView.swift
+//  DSKit
+//
+//  Created by choijunios on 8/15/24.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+/// 메인 화면의 탭바 아이템으로 사용됩니다.
+public class CAPTabBarItemView: TappableUIView {
+    
+    // Init
+    public let index: Int
+    public let idleImage: UIImage
+    public let accentImage: UIImage
+    
+    // State components
+    public enum State {
+        case idle
+        case accent
+    }
+    
+    public struct ImageForState {
+        let idleImage: UIImage
+        let accentImage: UIImage
+        public init(idleImage: UIImage, accentImage: UIImage) {
+            self.idleImage = idleImage
+            self.accentImage = accentImage
+        }
+    }
+    
+    // idle
+    let idleTextTStyle: TypographyStyle = .smallRegular
+    let idleTextColor: UIColor = DSColors.gray40.color
+    let idleBackgroundColor: UIColor = .clear
+    
+    
+    // accent
+    let accentTextTStyle: TypographyStyle = .smallBold
+    let accentTextColor: UIColor = DSColors.primary90.color
+    let accentBackgroundColor: UIColor = DSColors.gray5.color
+    
+    // View
+    let label: CapLabel = {
+        let label = CapLabel()
+        label.typographyStyle = .smallRegular
+        return label
+    }()
+    let image: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
+    public init(index: Int, labelText: String, imageSet: ImageForState) {
+        self.index = index
+        self.label.text = labelText
+        self.idleImage = imageSet.idleImage
+        self.accentImage = imageSet.accentImage
+        
+        super.init()
+        
+        setAppearance()
+        setLayout()
+    }
+    public required init?(coder: NSCoder) { nil }
+    
+    private func setAppearance() {
+        self.layer.cornerRadius = 44
+        self.clipsToBounds = true
+    }
+    
+    private func setLayout() {
+        let mainStack = VStack([image, label], spacing: 4, alignment: .center)
+        
+        self.addSubview(mainStack)
+        mainStack.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            mainStack.topAnchor.constraint(equalTo: self.topAnchor, constant: 12),
+            mainStack.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -16),
+            mainStack.leftAnchor.constraint(greaterThanOrEqualTo: self.leftAnchor, constant: 4),
+            mainStack.rightAnchor.constraint(lessThanOrEqualTo: self.rightAnchor, constant: -4),
+        ])
+    }
+    
+    /// 탭바 아이템의 상태를 변경합니다.
+    public func setState(_ state: State, duration: CGFloat = 0.2) {
+        UIView.animate(withDuration: duration) { [weak self] in
+            if state == .accent {
+                self?.setToAccent()
+            } else {
+                self?.setToIdle()
+            }
+        }
+    }
+    
+    private func setToIdle() {
+        label.attrTextColor = idleTextColor
+        label.typographyStyle = idleTextTStyle
+        image.image = idleImage
+        self.backgroundColor = idleBackgroundColor
+    }
+    
+    private func setToAccent() {
+        label.attrTextColor = accentTextColor
+        label.typographyStyle = accentTextTStyle
+        image.image = accentImage
+        self.backgroundColor = accentBackgroundColor
+    }
+}
---
+++ Modules/Presentation/DSKit/Source/Essence/DSKitShortcuts.swift
@@ -0,0 +1,10 @@
+//
+//  DSKitShortcut.swift
+//  DSKit
+//
+//  Created by choijunios on 8/15/24.
+//
+
+import Foundation
+
+public typealias DSColors = DSKitAsset.Colors
