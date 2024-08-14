ee4013d2919caced255073fbbc471f6007ce2584
J0onYEong <cjy2134@naver.com>
2024-08-15 01:35:59 +0900

    [CAP-002] CAPMainPage타입 생성

---
+++ Modules/Presentation/BaseFeature/Source/View/TabBar/CAPMainPage.swift
@@ -0,0 +1,71 @@
+//
+//  CAPMainPage.swift
+//  BaseFeature
+//
+//  Created by choijunios on 8/15/24.
+//
+
+import Foundation
+
+public enum CAPMainPage {
+    
+    public enum State {
+        case idle
+        case accent
+    }
+    
+    case home
+    case summaryAddtion
+    case categoryAddition
+    case environmentalSetting
+    
+    public init?(index: Int) {
+        switch index {
+        case 0: self = .home
+        case 1: self = .summaryAddtion
+        case 2: self = .categoryAddition
+        case 3: self = .environmentalSetting
+        default: return nil
+        }
+    }
+        
+    public func pageOrderNumber() -> Int {
+        switch self {
+        case .home:
+            return 0
+        case .summaryAddtion:
+            return 1
+        case .categoryAddition:
+            return 2
+        case .environmentalSetting:
+            return 3
+        }
+    }
+    
+    public func tabItemIcon(_ state: State) -> UIImage {
+        typealias Images = DSKitAsset.Images
+        switch self {
+        case .home:
+            return (state == .accent ? Images.tabIconHome1 : Images.tabIconHome0).image
+        case .summaryAddtion:
+            return (state == .accent ? Images.tabIconAddSum1 : Images.tabIconAddSum0).image
+        case .categoryAddition:
+            return (state == .accent ? Images.tabIconAddCat1 : Images.tabIconAddCat0).image
+        case .environmentalSetting:
+            return (state == .accent ? Images.tabIconSetting1 : Images.tabIconSetting0).image
+        }
+    }
+    
+    public func tabItemText() -> String {
+        switch self {
+        case .home:
+            "홈"
+        case .summaryAddtion:
+            "숏폼 추가"
+        case .categoryAddition:
+            "카테고리 추가"
+        case .environmentalSetting:
+            "환경설정"
+        }
+    }
+}
---
+++ Modules/Presentation/BaseFeature/Source/View/TabBar/CAPTabBarController.swift
@@ -0,0 +1,48 @@
+//
+//  CAPTabBarController.swift
+//  BaseFeature
+//
+//  Created by choijunios on 8/15/24.
+//
+
+import UIKit
+import UIKit
+import RxCocoa
+import RxSwift
+import Entity
+import DSKit
+
+public class CAPTabBarController: BaseVC {
+    
+    // Init
+    
+    // View
+    
+    // Observable
+    private let disposeBag = DisposeBag()
+    
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    public required init?(coder: NSCoder) { fatalError() }
+    
+    private func setAppearance() {
+        
+    }
+    
+    private func setLayout() {
+        
+    }
+    
+    private func setObservable() {
+        
+    }
+}
+
+
+// MARK: TabBar Flow
+extension CAPTabBarController {
+    
+    
+}
