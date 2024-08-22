f27965a45c9bf93e892e5668af773ecc7ba53bff
J0onYEong <cjy2134@naver.com>
2024-08-22 12:21:42 +0900

    [CAP-005] SummaryCell초기 버전 구햔

---
+++ Modules/Presentation/DSKit/Source/CommonUI/Loading/CAPLoadingIndicatorView.swift
@@ -0,0 +1,65 @@
+//
+//  CAPLoadingIndicatorView.swift
+//  DSKit
+//
+//  Created by choijunios on 8/22/24.
+//
+
+import UIKit
+
+public class CAPLoadingIndicatorView: UIView {
+    
+    let indicator = UIActivityIndicatorView(style: .medium)
+    
+    public init() {
+        super.init(frame: .zero)
+        setAppearance()
+        setLayout()
+    }
+    required init?(coder: NSCoder) { return nil }
+    
+    private func setAppearance() {
+        self.backgroundColor = DSColors.gray10.color.withAlphaComponent(0.5)
+        self.alpha = 0
+    }
+    
+    private func setLayout() {
+        self.addSubview(indicator)
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            indicator.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            indicator.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+        ])
+    }
+    
+    public func turnOn(withAnimation: Bool = true, duration: CGFloat = 0.2) {
+        self.indicator.startAnimating()
+        UIView.animate(withDuration: withAnimation ? duration : 0) {
+            self.alpha = 1
+        }
+    }
+    
+    public func turnOff(withAnimation: Bool = true, duration: CGFloat = 0.2) {
+        UIView.animate(withDuration: withAnimation ? duration : 0) {
+            self.alpha = 0
+        } completion: { _ in
+            self.indicator.stopAnimating()
+        }
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview("Preview", traits: .defaultLayout) {
+    
+    let view = CAPLoadingIndicatorView()
+    
+    DispatchQueue.main.asyncAfter(deadline: .now()+3) {
+        view.turnOn()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+2) {
+            view.turnOff()
+        }
+    }
+    
+    return view
+}
