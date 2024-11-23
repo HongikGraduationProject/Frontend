43d0ef06a9e51bea203063b0bc35d402237c5f4d
J0onYEong <cjy2134@naver.com>
2024-11-24 00:12:29 +0900

    [CAP-007] ScrollingLabel구현

---
+++ Modules/Presentation/CommonUI/Source/ScrollingLabel.swift
@@ -0,0 +1,90 @@
+//
+//  ScrollingLabel.swift
+//  Shortcap
+//
+//  Created by choijunios on 11/23/24.
+//
+
+import UIKit
+
+import DSKit
+
+public class ScrollingLabel: UIScrollView {
+    
+    private let label: CapLabel
+    
+    public private(set) var isScrolling = false
+    
+    public init(label: CapLabel) {
+        
+        self.label = label
+        
+        super.init(frame: .zero)
+        
+        setScrollView()
+        setLayout()
+    }
+    required init?(coder: NSCoder) { nil }
+    
+    private func setScrollView() {
+        
+        self.showsHorizontalScrollIndicator = false
+        self.showsVerticalScrollIndicator = false
+        self.isScrollEnabled = false
+    }
+    
+    private func setLayout() {
+        
+        self.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        let contentGuide = self.contentLayoutGuide
+        let frameGuide = self.frameLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            
+            label.topAnchor.constraint(equalTo: frameGuide.topAnchor),
+            label.leftAnchor.constraint(equalTo: contentGuide.leftAnchor),
+            label.rightAnchor.constraint(equalTo: contentGuide.rightAnchor),
+            label.bottomAnchor.constraint(equalTo: frameGuide.bottomAnchor),
+            
+            self.widthAnchor.constraint(equalToConstant: 100)
+        ])
+    }
+    
+    public func stopScrolling() {
+        
+        self.isScrolling = false
+        self.layer.removeAllAnimations()
+        self.contentOffset = .zero
+    }
+    
+    public func startScrolling(duration: CGFloat) {
+        
+        if isScrolling { return }
+        isScrolling = true
+        
+        self.layoutIfNeeded()
+        
+        let originWidth = label.intrinsicContentSize.width
+        let currentWidth = self.frame.width
+        let distance = originWidth - currentWidth
+        
+        UIView.animateKeyframes(
+            withDuration: duration,
+            delay: 0,
+            options: [
+                .repeat,
+                .calculationModeLinear,
+                .autoreverse
+            ],
+            animations: {
+                
+                UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1.0) {
+                    
+                    self.contentOffset = .init(x: distance, y: 0)
+                }
+            }
+        )
+    }
+}
